844c2e49f9a4670a85501daa03b1a553d2289324
pt-do <pedro.turati@gmail.com>
2025-05-09 08:45:49 -0300

    Test commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#Finite Difference Time Domain implementation for 1-dimensional electromagnetic simulations
+#This project is intented as a study exercise in CEM (Computional Electromagnetism)
+#Autor: Pedro Turati de Oliveira
+
